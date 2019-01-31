0db2588bd6086db66795185e594372e741a76aa3
djiro-256 <info@d-nakazawa.com>
2019-01-31 22:30:34 +0900

    add migrations

--- project/diary/models.py
+++ project/diary/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.utils import timezone #djangoではdatetime.nowのかわりに、timezone.nowで現在日付・時刻を取得する
 
-# Create your models here.
+class Day(models.Model):
+    title = models.CharField('タイトル',max_length=200)
+    text = models.TextField('本文')
+    date = models.DateTimeField('日付', default=timezone.now)
--- project/diary/urls.py
+++ project/diary/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'diary'
 
 urlpatterns = [
-    path('', views.index, name='index'), #/diaryで表示
+    path('', views.index, name='index'),#/diaryで表示
+    path('add/', views.add, name='add'),#/diary/add
 ]
